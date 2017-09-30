d29b2f48a3ccf9740561b7521897404a19c82fd8
tobire <mail@tobias-reinke.de>
2017-09-30 14:24:18 +0200

    began adding playlist

--- app.js
+++ app.js
@@ -33,6 +33,11 @@ app.get("/", function (req, res) {
         { name: "User 2"},
         { name: "User 3"},
     ];
+    res.locals.upcoming = [
+        { id: "tdUX3ypDVwI", title: "Making Life Multiplanetary"},
+        { id: "bvim4rsNHkQ", title: "How Not to Land an Orbital Rocket Booster"},
+        { id: "zqE-ultsWt0", title: "BFR | Earth to Earth"},
+    ];
 
     res.render("index");
 });
