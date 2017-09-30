55fafdbab898574a3cabe572e94af84d9de794cc
tobire <mail@tobias-reinke.de>
2017-09-30 22:34:11 +0200

    client side socket.io

--- public/js/vync.js
+++ public/js/vync.js
@@ -1,6 +1,42 @@
-/* global $:true */
+/* global $:true io:true */
 
 $(document).ready(function() {
     var height = ($("#content #player").css("width").replace("px", "") * 0.5625) + "px";
     $("#content #player").css("height", height);
 });
+
+var socket = io();
+socket.on("play", () => this.player.play());
+socket.on("pause", () => this.player.pause());
+socket.on("seek", (time) => this.player.setCurrentTime(time));
+socket.on("setVideo", (platform, videoId) => this.player.playback(platform, videoId));
+
+socket.on("userupdate"), (data) => updateUserlist(data);
+
+function updateUserlist(users) {
+    $("#users ul").each(function() {
+        var item = getByProperty(users, "id", $(this).id);
+        if (item) {
+            users.remove(item);
+        } else {
+            $(this).remove();
+        }
+    });
+    $.each(users, function(item) {
+        $("#users ul").append(createUserItem(item));
+    });
+}
+
+function getByProperty(haystack, property, needle) {
+    $.each(haystack, function(value) {
+        if (value[property] == needle)
+            return value;
+    });
+}
+
+function createUserItem(user) {
+    return "<li id=\"" + user.id + "\" class=\"list-group-item\">" +
+                "<span>" + user.name + "</span>" +
+                "<i class=\"fa fa-times-circle-o\" aria-hidden=\"true\" title=\"Kick\"></i>" +
+            "</li>";
+}
