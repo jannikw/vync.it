7a277ef2617304184e7c2ea799fd63a81bad923c
tobire <mail@tobias-reinke.de>
2017-09-30 14:34:57 +0200

    libs now local

--- public/js/vync.js
+++ public/js/vync.js
@@ -1,6 +1,6 @@
 /* jQuery:true */
 
 $(document).ready(function() {
-    var height = ($("#content #player").css('width').substring(0, 3) * 0.5625) + 'px';
+    var height = ($("#content #player").css('width').replace('px', '') * 0.5625) + 'px';
     $("#content #player").css('height', height);
 });
