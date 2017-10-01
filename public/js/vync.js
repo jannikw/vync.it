95ee7dd9ce112a0e74839bd46cb5fa3d948db651
tobire <mail@tobias-reinke.de>
2017-10-01 04:44:53 +0200

    emitting local events

--- public/js/vync.js
+++ public/js/vync.js
@@ -15,12 +15,14 @@ Vync = {
     })()
 };
 
+/* Page loading */
 $(document).ready(function() {
     var height = ($("#content #player").width() * 0.5625) + "px";
     $("#content #player").css("height", height);
     Vync.socket.emit("enter", Vync.lobbyId);
 });
 
+/* Remote events */
 Vync.socket.on("play", () => this.player.play());
 Vync.socket.on("pause", () => this.player.pause());
 Vync.socket.on("seek", (time) => this.player.setCurrentTime(time));
@@ -29,6 +31,30 @@ Vync.socket.on("confirmName", (name) => updateOwnName(name));
 
 Vync.socket.on("userupdate", (data) => updateUserlist(data));
 
+/* Local player events */
+this.player.on("timeupdate", (data) => {
+    Vync.socket.emit("timeupdate", data);
+});
+this.player.on("play", () => {
+    Vync.socket.emit("play");
+});
+this.player.on("pause", () => {
+    Vync.socket.emit("pause");
+});
+this.player.on("buffering", () => {
+    Vync.socket.emit("buffering");
+});
+this.player.on("ended", () => {
+    Vync.socket.emit("ended");
+});
+this.player.on("ready", () => {
+    Vync.socket.emit("ready");
+});
+this.player.on("error", (data) => {
+    Vync.socket.emit("error", data);
+});
+
+/* Change name dialog */
 $("#renameModal").on("show.bs.modal", function(){
     $("#newName").val("");
 });
@@ -41,6 +67,7 @@ function changeName() {
     $("#renameModal").modal("hide");
 }
 
+/* User list update */
 function updateOwnName(name) {
     $("#self span").val(name);
 }
